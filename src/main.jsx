f1351845c1f8ec158159e3ccfd5dcf8c2ca984b0
pinerolly0578 <pinerolly05@gmail.com>
2016-09-07 20:38:37 -0400

    basic start react

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var ReactDOM = require('react-dom');
+var List = require('./component/List.jsx');
+var React = require('react');
+
+ReactDOM.render(<List />, document.getElementById('ingredients'));
